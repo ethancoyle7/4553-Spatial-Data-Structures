4702b5934fcd7aa6c3ba0dd40a3f60c68c6d4f0e
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2022-01-27 00:57:20 -0600

    editting

--- Assignments/P01/project.py
+++ Assignments/P01/project.py
@@ -1,61 +1,149 @@
-######################################################
-## Student - Ethan Coyle                            ##
-## Instr.  - Dr. Griffin                            ##
-## Class   - Spatial Data Structures                ##
-## Assign. - Project 1                              ##
-##Overview - The purpose of this program is to      ##
-##           Read in a json file containing cities  ##
-##           with long,lats, growth and convert to  ##
-##           a geojson format for mapping out       ##
-##         find highest population state order(1-49)##
-##   sort cities west to east on the x coordingate  ##
-##   and number 
-######################################################
-
 import json
-### from rich import print
-import random
-
-def randColor():
-  r = lambda: random.randint(0,255)
-  print('#%02X%02X%02X' % (r(),r(),r()))
+import random as rand
+from rich import print, console
 
 
-def makeFeature(city):
-  feature = {
-    "type": "Feature",
-    "properties": {},
-    "geometry": {
-      "type": "Point",
-      "coordinates": [0,0]
-    }
-  }
+# create a class to read the input data and do some conversions with it
+class OrderedCities:
+    ## contructor for the class orderedcities will be ranked eventually
+    # from the east coast to the west coast according to the population
+    def __init__(self):
+        self.StateName = {}
+        self.MaxPopulationCities = {}
+        self.CityRank = []
+        self.GeoJsonList = []
 
-  for key,val in city.items():
-    if key == 'latitude':
-      feature['geometry']['coordinates'][1] = val
-    elif key == 'longitude':
-      feature['geometry']['coordinates'][0] = val
-    else:
-      feature['properties'][key] = val
+    # function definintion to read the input file 
+    # we also need to check for invalid file types so we read the 
+    # input in a try except to throw errors if we have issues reading inpuet
+    def ReadInput(self):
+        # try reading the input file from the given file
+        try:
+            with open('Assignments/P01/testdata.json', 'r') as data:
+                # assign the json loaded data into a file called InputData for use
+                InputData = json.load(data)
+        # if cant then toss an error
+        except EnvironmentError as e:
+            print(f'Error, unable to open the specified folder: Error Type = {e}')
+        # for each city in data
+        for city in InputData:
+            if not city['state'] in self.StateName:
+                state = city['state']
+                self.StateName[state] = []
+            self.StateName[city['state']].append(city)
 
-  return feature
+    # function definition to filter through the cities and the items
+    # will determine the cities by their population
+    def FilterCity(self):
+        for state, InputData in self.StateName.items():
+            max = -1 # default max value
+            for data in InputData:
+                # where the data falls withing the long and lat lines
+                if data['population'] > max and data['longitude'] < 110 \
+                        and data['latitude'] < 50 and data['latitude'] > 25:
+                    max = int(data['population'])
+                    self.MaxPopulationCities[state] = data
+            # for data, value in self.MaxPopulationCities.items():
+            #     print(data, value)
 
-with open("Assignments/P01/testdata.json") as f:
-  data = json.load(f)
+    # function definition to parse through the cities by population a
+    # will rank them according to the values low to high
+    def rank_cities(self):
+        
+        for key, value in self.MaxPopulationCities.items():
+            self.CityRank.append(value)
 
-states = {}
+        # Sort ranked cities by population, this will allow us to use the
+        # ranking as the marker for GeoJson
+        # cities ranked by the population
+        # allows for easy utilization in the transferral to proper geojson 
+        # format
 
-for item in data:
-  if not item["state"] in states:
-    states[item["state"]] = []
+        self.CityRank = sorted(self.CityRank,key=lambda data: data['population'])
 
-  states[item["state"]].append(item)
+        # Set the ranking of the Cities, will begin at 1 being the smallest
+        num = 1
+        # for each item inside of this ranked cities assign a number
+        for item in self.CityRank:
+            item['rank'] = num
+            num += 1 
+            # ranking them by number 
+        # print out the sorted ranked list
+        print(self.CityRank)
 
+    # function definition for the color generation
+    # here we have red green and blue colors
+    # we are integrating a random selection for each one of the colors t
+    # then we are returning that random color back
+    def ColorGeneration(self):
+        # setting the colors to random for rgb color schematic
+        # each color is chosing a random integer between 0 and 255
+        Red = lambda: rand.randint(0,255)
+        Green = lambda: rand.randint(0,255)
+        Blue = lambda: rand.randint(0,255)
+        # return the formatted colors in formatted string
+        return f'#%02X%02X%02X' % (Red(),Blue(),Green())
 
-for state in states:
-  print(f"{state} = {len(states[state])}")
+    # to convert to a geo json style format 
+    def FileConversion(self):
+        self.GeoJsonList = {
+            "type": "FeatureCollection",
+            "features": []
+            # have type feature collection and then added features
+            # including long and lats etc.
+        }
+        for InputData in self.CityRank:
+            # get the color of the marker, the size and the symbol
+            InputData['marker-color'] = self.ColorGeneration()
+            InputData['marker-size'] = "small"
+            InputData['marker-symbol'] = InputData['rank']
 
-for stateInfo in data:
-  print(makeFeature(stateInfo))
-  
+            # append the features of each city for nice usng
+            self.GeoJsonList['features'].append({
+                "type": "Feature",
+                "properties": InputData,
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        InputData['longitude'],
+                        InputData['latitude'],
+                    ]}})
+        # Sorting by longitudes 
+        # Property is argument and looking at property longitude
+        self.CityRank = sorted(self.CityRank,key=lambda Property: Property['longitude'])
+        print(self.CityRank)
+       
+        for i in range(len(self.CityRank)):
+            # assigning the rank if it is not in the length-1 or else
+            
+            if_i = i+1 if i is not (len(self.CityRank) - 1) else i
+            self.GeoJsonList['features'].append(
+                {
+                    "type": "Feature",
+                    "properties":{
+                        "stroke": self.ColorGeneration(),
+                        "stroke-width": 2,
+                    },
+                    "geometry":{
+                        "type": "LineString",
+                        "coordinates":[
+                            [self.CityRank[i]['longitude'],
+                            self.CityRank[i]['latitude']],
+                            [self.CityRank[if_i]['longitude'],
+                             self.CityRank[if_i]['latitude']],
+                        ]
+                    }
+                }
+            )
+        
+        with open('Assignments/P01/outputfile.json', 'w') as file:
+            # open up the output file and then dump the conversion inside
+            file.write(json.dumps(self.GeoJsonList, indent=4))
+
+if __name__ == '__main__':
+    # object called cities of the OrderedCities class
+    Cities = OrderedCities()
+    Cities.ReadInput()
+    Cities.FilterCity()
+    Cities.rank_cities()
+    Cities.FileConversion()
