11cc790e63ab0be364f344a572a2229f9c5849c1
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2022-02-15 23:02:28 -0600

    fixing

--- Assignments/P02/main.py
+++ Assignments/P02/main.py
@@ -22,63 +22,75 @@
 # creating our inports
 import pandas as pd
 import json
-
+## geopandas isnt wanting to download so in order to complete run the
+## program in a way such that will compare the citites.geojson to 
+## and it longitudes and latitudes and compare values
 
 
 #import geopandas as gpd
 # first read in the dataframe with the ufo sightings and print out 
 # the head to make sure that it is reading properly
-df = pd.read_csv('Assignments/P02/UFOSightings.csv')
-print(df.head(20))
+df1 = pd.read_csv('Assignments/P02/UFOSightings.csv')
+print(df1.head(20))
 
+# read in the data associated with the bounding boxes of the united states
+# states
+df2= pd.read_csv('Assignments/P02/unitedstatesboundingboxes.csv')
+print(df2.head(10))
+print(df2['state']) # this will be the value of comparison to the data
+                     # inside of the csv file of cities
 
+# given the merge, we merge the two together to merge the data frames together 
+# on the state name and create a new data frame that holds the values
+# including the max  and mins of the x and y
+df3 = pd.merge(df1, df2,
+                   on='state',
+                   how='right')
+print('output now is :\n', df3)
 # to make easier to look at this lets drop uneccesary collums
 # make the implace to be true so we dont have to worrry
 # if not then we have to assign the changes to new dataframe
-df.drop(['shape','duration','date_time'], axis=1,inplace=True)
-print("Our new dataframe is :n\n", df)
+df3.drop(['shape','duration','date_time'], axis=1,inplace=True)
+print("Our new dataframe is :n\n", df3)
 
 
 # initialize the bounding box for the united states
-top = 49.3457868 # north lat
-leftborder = -124.7844079 # west long
-rightborder = -66.9513812 # east long
-bottom =  24.7433195 # south lat
+
 
 # drop uneccesary of the left bounding box border of us both past or before 
-# based on the top and bottom vals
-df = df.drop(df[(df['lon'] <= leftborder) & (df['lat'] <= bottom)].index)
-df = df.drop(df[(df['lon'] <= leftborder) & (df['lat'] >= top)].index)
-df = df.drop(df[(df['lon'] >= leftborder) & (df['lat'] <= bottom)].index)
-df = df.drop(df[(df['lon'] >= leftborder) & (df['lat'] >= top)].index)
+# based on the df3['ymax'] and df3['ymin'] vals
+df3 = df3.drop(df3[(df3['lon'] <= df3['xmin']) & (df3['lat'] <= df3['ymin'])].index)
+df3 = df3.drop(df3[(df3['lon'] <= df3['xmin']) & (df3['lat'] >= df3['ymax'])].index)
+df3 = df3.drop(df3[(df3['lon'] >= df3['xmin']) & (df3['lat'] <= df3['ymin'])].index)
+df3 = df3.drop(df3[(df3['lon'] >= df3['xmin']) & (df3['lat'] >= df3['ymax'])].index)
 
 # drop uneccesary of the right bounding box border of us both past or before 
-# based on the top and bottom vals
-df = df.drop(df[(df['lon'] >= rightborder) & (df['lat'] <= bottom)].index)
-df = df.drop(df[(df['lon'] >= rightborder) & (df['lat'] >= top)].index)
-df = df.drop(df[(df['lon'] <= rightborder) & (df['lat'] <= bottom)].index)
-df = df.drop(df[(df['lon'] <= rightborder) & (df['lat'] >= top)].index)
+# based on the df3['ymax'] and df3['ymin'] vals
+df3 = df3.drop(df3[(df3['lon'] >= df3['xmax']) & (df3['lat'] <= df3['ymin'])].index)
+df3 = df3.drop(df3[(df3['lon'] >= df3['xmax']) & (df3['lat'] >= df3['ymax'])].index)
+df3 = df3.drop(df3[(df3['lon'] <= df3['xmax']) & (df3['lat'] <= df3['ymin'])].index)
+df3 = df3.drop(df3[(df3['lon'] <= df3['xmax']) & (df3['lat'] >= df3['ymax'])].index)
 
 
 # want to test the number of occurances in each state
 print("The number of occurances in each state are :\n")
-print(df['state']. value_counts())
+print(df3['state']. value_counts())
 # number of occurances in city data
 print("The number of occurances in each city  are :\n")
-print(df['city']. value_counts()) 
+print(df3['city']. value_counts()) 
 
-df['lat'] = df['lat'].astype(float)
-df['lon'] = df['lon'].astype(float)
-df['state'] = df['state'].str.title()
-df['city'] = df['city'].str.title()
+df3['lat'] = df3['lat'].astype(float)
+df3['lon'] = df3['lon'].astype(float)
+df3['state'] = df3['state'].str.title()
+df3['city'] = df3['city'].str.title()
 
 # convert our dataframe to a gejson
-def df_to_geojson(df, properties, lat='lat', lon='lon'):
+def df_to_geojson(df3, properties, lat='lat', lon='lon'):
     # create a new python dict to contain our geojson data, using geojson format
     geojson = {'type':'FeatureCollection', 'features':[]}
 
     # loop through each row in the dataframe and convert each row to geojson format
-    for _, row in df.iterrows():
+    for _, row in df3.iterrows():
         
         # create a feature template to fill in
         feature = {'type':'Feature',
@@ -99,7 +111,7 @@ def df_to_geojson(df, properties, lat='lat', lon='lon'):
 
 cols = ['state', 'city', 'lat', 'lon']
 # call to create the geojson stuff
-geojson = df_to_geojson(df, cols)
+geojson = df_to_geojson(df3, cols)
 # for visualization print out the geojson to terminal
 print(geojson)
 
