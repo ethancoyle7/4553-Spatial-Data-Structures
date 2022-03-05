50e41ad799f28c7dd358c0e7544ddddbc7e385bd
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2022-03-05 00:09:44 -0600

    commets and cleanup

--- Assignments/P03/main.py
+++ Assignments/P03/main.py
@@ -20,18 +20,21 @@ import matplotlib
 matplotlib.use('TkAgg') ## required for replit to properly run the GUI because of prerequisites from matplot lib
 import matplotlib.pyplot as plt
 
+
+# user notes  if geovornoi is working on IDE, then the relative path
 from shapely.ops import unary_union
 from geovoronoi import voronoi_regions_from_coords, points_to_coords
 from geovoronoi.plotting import subplot_for_map, plot_voronoi_polys_with_points_in_area
-df1 = pd.read_csv("cities.csv")
+df1 = pd.read_csv("Assignments\P03\cities.csv")
 
 # geopandas data frame from a geojson file\n",
-gdf2 = gpd.read_file('cities.geojson')
+gdf2 = gpd.read_file('Assignments\P03\cities.geojson')
 
 gdf1 = gpd.GeoDataFrame(df1, geometry=gpd.points_from_xy(df1.lon, df1.lat))
 # viewing the head of the geo dataframe
 gdf1.head()
-"#boundary = gpd.read_file(\"data/us_nation_border.geojson\")\n",
+
+# read in the border shape file for the united states 
 boundary = gpd.read_file("us_border.shp")
 fig, ax = plt.subplots(figsize=(12, 10))
 boundary.plot(ax=ax, color= "gray")
@@ -39,11 +42,7 @@ gdf2.plot(ax=ax, markersize=2.5, color="blue")
 ax.axis("off")
 plt.axis('equal')
 
-#minx, miny, maxx, maxy = gdf2.total_bounds
-#print(minx,miny,maxx,maxy)
-#ax.set_xlim(minx-5, maxx+5)
-#ax.set_ylim(miny, maxy)
-#plt.show()
+
 boundary = boundary.to_crs(epsg=3395)
 gdf_proj = gdf2.to_crs(boundary.crs)
 boundary_shape = unary_union(boundary.geometry)
@@ -54,6 +53,8 @@ fig, ax = subplot_for_map(figsize=(12, 10))
 plot_voronoi_polys_with_points_in_area(ax, boundary_shape, region_polys, coords, region_pts)
 plt.show()
 
+# lets see the polygon  vals
 for i,poly in region_polys.items():
     print(poly)
-plt.savefig("graph.png")
+# save the plot figure to a file
+plt.savefig("Assignments\P03\graph.png")
