aa2d7b96257677f70f99a587285fd69cf5b86f4e
ethancoyle7 <77364000+ethancoyle7@users.noreply.github.com>
2022-03-20 13:57:41 -0500

    fixed po3 with rtree

--- Assignments/P03/main.py
+++ Assignments/P03/main.py
@@ -1,5 +1,3 @@
-# pandas data frame from cites in a csv\n",
-
 #################################################################
 ## Name - Ethan Coyle                                           #
 ## Inst.- Dr. Griffin                                           #
@@ -9,53 +7,136 @@
 ## the cities.csv file and the data for the gities.geojson and  #
 ## create a voronoi of the ufo data and the city boundaries     #
 #################################################################
-## vscode does not like geovoroni in the terminal even in 
-## geo environment so I had to run this in the replit  
-# link to replit running plot of diagram
-# https://replit.com/@ethancoyle71/anotherone#main.py
-import numpy as np
-import pandas as pd
-import shapely
-import json
-import geopandas as gdp
-import matplotlib
-matplotlib.use('TkAgg') ## required for replit to properly run the GUI because of prerequisites from matplot lib
-import matplotlib.pyplot as plt
 
+########################################################
+# ██╗███╗   ███╗██████╗  ██████╗ ██████╗ ████████╗███████╗ #
+# ██║████╗ ████║██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝  #
+# ██║██╔████╔██║██████╔╝██║   ██║██████╔╝   ██║   ███████╗ #
+# ██║██║╚██╔╝██║██╔═══╝ ██║   ██║██╔══██╗   ██║   ╚════██║ #
+# ██║██║ ╚═╝ ██║██║     ╚██████╔╝██║  ██║   ██║   ███████║ #
+# ╚═╝╚═╝     ╚═╝╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝ #
+#########################################################
+import numpy as np # our numpy array read in as np
+import pandas as pd # our pandas read in as pd
+import geopandas as gdp # geopandas dataframs as gdp
+import json # for the json output
+import shapely # for the shapeley diagram stuff
+import matplotlib.pyplot as plt # plotting out the data
 
-# user notes  if geovornoi is working on IDE, then the relative path
+# other imports for our data sing geovornoi for the voronoi diagram
 from shapely.ops import unary_union
 from geovoronoi import voronoi_regions_from_coords, points_to_coords
 from geovoronoi.plotting import subplot_for_map, plot_voronoi_polys_with_points_in_area
-df1 = pd.read_csv("Assignments\P03\cities.csv")
 
-# geopandas data frame from a geojson file\n",
-gdf2 = gdp.read_file('Assignments\P03\cities.geojson')
-gdf1 = gdp.GeoDataFrame(df1, geometry=gdp.points_from_xy(df1.lon, df1.lat))
-# viewing the head of the geo dataframe
-gdf1.head()
+##########################################################
+# ███████╗██╗██╗     ███████╗███████╗    ██╗    ██╗ ██████╗  #
+# ██╔════╝██║██║     ██╔════╝██╔════╝    ██║   ██╔╝██╔═══██╗ #
+# █████╗  ██║██║     █████╗  ███████╗    ██║  ██╔╝ ██║   ██║ #
+# ██╔══╝  ██║██║     ██╔══╝  ╚════██║    ██║ ██╔╝  ██║   ██║ #
+# ██║     ██║███████╗███████╗███████║    ██║██╔╝   ╚██████╔╝ #
+# ╚═╝     ╚═╝╚══════╝╚══════╝╚══════╝    ╚═╝╚═╝     ╚═════╝  #
+###########################################################
+                                                          
+#We need to open up our file to be read in and used inside of main
+try:
+  USBorderShape=gdp.read_file("us_border.shp") # us border shape file
+except IOError:
+  print("the data file wasnt read in please try another data file name")
+try:
+  GeoJsonCities = gdp.read_file("cities.geojson") # read in the city data from the geojson file
+except IOError:
+  print("the data file was not read in properly might wanna check the name")
+try:
+  UFOData = pd.read_csv("UFOData.csv")
+  # reda in the ufo sightings to geopandas df and read in that gdp lon and lat
+  UFOData = gdp.GeoDataFrame(UFOData, geometry=gdp.points_from_xy(UFOData.lon, UFOData.lat))
+except IOError:
+  print("hmm that data for the ufo has mysteriously gone missing ")
 
-# read in the border shape file for the united states 
-boundary = gdp.read_file("us_border.shp")
-fig, ax = plt.subplots(figsize=(12, 10))
-boundary.plot(ax=ax, color= "gray")
-gdf2.plot(ax=ax, markersize=2.5, color="blue")
-ax.axis("off")
-plt.axis('equal')
+#################################################################
+# ██████╗ ██╗      ██████╗ ████████╗████████╗██╗███╗   ██╗ ██████╗  #
+# ██╔══██╗██║     ██╔═══██╗╚══██╔══╝╚══██╔══╝██║████╗  ██║██╔════╝   #
+# ██████╔╝██║     ██║   ██║   ██║      ██║   ██║██╔██╗ ██║██║  ███╗ #
+# ██╔═══╝ ██║     ██║   ██║   ██║      ██║   ██║██║╚██╗██║██║   ██║ #
+# ██║     ███████╗╚██████╔╝   ██║      ██║   ██║██║ ╚████║╚██████╔╝  #
+# ╚═╝     ╚══════╝ ╚═════╝    ╚═╝      ╚═╝   ╚═╝╚═╝  ╚═══╝ ╚═════╝  #
+  ################################################################
+                                                                 
+#create the size of the plot for our voronoi diagram
+fig, ax = subplot_for_map(figsize=(15, 15))
+# shape of the border and the coordinates and the geometry for vornoi diagram
+border_proj = GeoJsonCities.to_crs(USBorderShape.crs)
+ShapeOfBorder = unary_union(USBorderShape.geometry)
+Coordinates = points_to_coords(border_proj.geometry)
+PolyRegions,RegionPoints = voronoi_regions_from_coords(Coordinates, ShapeOfBorder)
 
-# creating the boundary for the united states reading the boundary crs file
-boundary = boundary.to_crs(epsg=3395)
-gdf_proj = gdf2.to_crs(boundary.crs)
-boundary_shape = unary_union(boundary.geometry)
-coords = points_to_coords(gdf_proj.geometry)
-region_polys, region_pts = voronoi_regions_from_coords(coords, boundary_shape)
-fig, ax = subplot_for_map(figsize=(12, 10))
-#gdf1.plot(ax=ax, markersize=2.5, color="blue")
-plot_voronoi_polys_with_points_in_area(ax, boundary_shape, region_polys, coords, region_pts)
-plt.show()
-plot_voronoi_polys_with_points_in_area(ax, boundary_shape, region_polys, coords, region_pts)
-# lets see the polygon  vals
-for i,PolyGonsData in region_polys.items():
-    print(PolyGonsData)
-#used to append dictionary
-plt.savefig("Assignments\P03\graph.png")
+RegionLength = len(PolyRegions)
+Length=RegionLength
+#We need to  create a geopandas series for the regins in our polygon to use with the rtree
+rtree = gdp.GeoSeries(PolyRegions)
+
+# plot the vornoi with the axis, the border shape, the polygon regions, regionpts and the coordinates
+plot_voronoi_polys_with_points_in_area(ax, ShapeOfBorder, PolyRegions, Coordinates, RegionPoints)
+# save the figure as graph.png
+plt.savefig("graph.png")
+
+# create some empty list for our output and the list of points to be used inside of main
+OutFileList = []
+PointList = []
+##################################
+# ███╗   ███╗ █████╗ ██╗███╗   ██╗ #
+# ████╗ ████║██╔══██╗██║████╗  ██║ #
+# ██╔████╔██║███████║██║██╔██╗ ██║  #
+# ██║╚██╔╝██║██╔══██║██║██║╚██╗██║  #
+# ██║ ╚═╝ ██║██║  ██║██║██║ ╚████║  #
+# ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝  #
+###################################                                
+
+# for  the polygon data in the range of the lenngth of the polygon region
+for PolyGonData in range(0, Length):
+    #makes sure the right PolyGonData is used so output is accurate
+    if shapely.geometry.polygon.Polygon ==type(rtree[PolyGonData]):
+        PolygonType = 'SinglePolyGon'
+        PointsInPolyGon = np.asarray(rtree[PolyGonData].exterior.coords)
+        PointsInPolyGon = PointsInPolyGon.tolist() # append coords to polygon point list
+    else:
+        PolygonType = 'MultiPolyGon'
+        PointsInPolyGon = [] # empty list for out points in our polygon
+        for data in rtree[PolyGonData]: # for the data inside of our rtree polygon
+            Coordinates = np.asarray(data.exterior.coords) # convert the coordinates to numpy array of coords
+            Coordinates = Coordinates.tolist()
+            PointsInPolyGon.append(Coordinates) # append the points to the polygon point list
+
+    #we need to get all the ufo data through a query base 
+    #DataQuery spatial index of all geometries in the tree with extents that intersect the rtree data polgyondata
+    DataQuery = rtree.sindex.query(rtree[PolyGonData])
+
+    # ensure that we pass back tohe polygon points and not the border
+    for CoordinatePoints in DataQuery:
+        if type(rtree[CoordinatePoints]) == shapely.geometry.point.Point:
+            # append the x and y coordinate points to the point list for each polygon
+            PointList.append([rtree[CoordinatePoints].x, rtree[CoordinatePoints].y])
+    # output list to hold the key value pairs in json for the type, points in the polygon and the list of points
+    OutFileList.append({
+        'Polygon Type ': PolygonType,
+        'Polygon Points': PointsInPolyGon,
+        'Point List': PointList
+    })
+#####################################################
+#  ██████╗ ██╗   ██╗████████╗██████╗ ██╗   ██╗████████╗ #
+# ██╔═══██╗██║   ██║╚══██╔══╝██╔══██╗██║   ██║╚══██╔══╝ #
+# ██║   ██║██║   ██║   ██║   ██████╔╝██║   ██║   ██║   #
+# ██║   ██║██║   ██║   ██║   ██╔═══╝ ██║   ██║   ██║   #
+# ╚██████╔╝╚██████╔╝   ██║   ██║     ╚██████╔╝   ██║   #
+#  ╚═════╝  ╚═════╝    ╚═╝   ╚═╝      ╚═════╝    ╚═╝   #
+  ###################################################
+                                                     
+# try opening the output file
+try:
+  # try opening file to filename as writeable to the outfile
+  with open('PointsInPolys.json', 'w') as outfile:
+      outfile.write(json.dumps(OutFileList,indent=4))
+except IOError:
+  print("hmm did ufos take your dat file because there is an error")
+finally:
+  print("we are all done now good bye chap")
